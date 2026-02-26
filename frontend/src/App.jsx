59eebe3858264141c68317ba42b8f51d3e651b44
kanchan kapri <kaprikanchan09@gmail.com>
2026-02-26 13:49:52 +0530

    Fixex the folder structure and App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Hero from './pages/Hero'
+import Content from './pages/Content'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 function App() {
   return (
-    <>
-      
+    <>   
       <Navbar/>
       <main>
         <Hero/>
---
+++ frontend/src/pages/Content.jsx
@@ -0,0 +1,7 @@
+function Content() {
+    return(
+        <h1> Your GovtSathi in content section</h1>
+    )
+}
+
+export default Content;
